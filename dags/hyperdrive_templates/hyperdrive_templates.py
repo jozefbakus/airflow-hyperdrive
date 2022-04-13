bcae105e36ae86b3375b928576a9f2fabc246658
jozefbakus <jozef.bakus@gmail.com>
2022-04-13 14:39:31 +0200

    Update hyperdrive_templates.py
    Fixed issue with DummyOperator

--- dags/hyperdrive_templates/hyperdrive_templates.py
+++ dags/hyperdrive_templates/hyperdrive_templates.py
@@ -4,11 +4,11 @@ from hyperdrive_spark_submit_operator.hyperdrive_spark_submit_operator import Hy
 
 #Place to merge template and user input properties
 
-def build_hyperconfornamce_task(dag: DAG) -> DummyOperator:
+def build_hyperconfornamce_task(dag: DAG) -> HyperdriveSparkSubmitOperator:
     hyperconformance = HyperdriveSparkSubmitOperator(task_id="hyperconformance", name="hyperconformance", dag=dag)
     return hyperconformance
 
-def build_offload_task(dag: DAG) -> DummyOperator:
+def build_offload_task(dag: DAG) -> HyperdriveSparkSubmitOperator:
     offload = HyperdriveSparkSubmitOperator(task_id="offload", name="offload", dag=dag)
     return offload
 
